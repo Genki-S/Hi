5f19effffd29463b1718af11362cb55a3413ae27
Shuhei TAKASUGI <bd.c211@gmail.com>
2014-11-09 07:06:52 +0900

    ボタンのアイコンをDiv要素に変更
    * 画像をクリックしてもボタンが反応するようにした
    * JSを少しだけ綺麗にした

--- server/public/js/send-interactions.js
+++ server/public/js/send-interactions.js
@@ -1,29 +1,17 @@
-function LikeSender(button, socket) {
+function LikeSender(button) {
   // button is document.getElementById('hoge') object
   this.button = button;
-  // socket is socket.io object
-  this.socket = socket;
 
   this.button.onclick = function(){
-    // var el = document.createElement('div');
-    // el.innerHTML = "like";
-    // this.parentNode.appendChild(el);
-    // this.socket.sendSomething("like", "like");
     hiSocket.emit("like", "like");
   };
 }
 
-function HiSender(button, socket) {
+function HiSender(button) {
   // button is document.getElementById('hoge') object
   this.button = button;
-  // socket is socket.io object
-  this.socket = socket;
 
   this.button.onclick = function(){
-    // this.socket.sendSomething("hi", "Hi");
     hiSocket.emit("hi", "Hi");
-    // var el = document.createElement('div');
-    // el.innerHTML = "Hi!";
-    // this.parentNode.appendChild(el);
   };
 }
