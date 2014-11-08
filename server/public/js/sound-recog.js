f02f5201aaf09d64a5e89889f3eee769a1449c5d
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-11-08 22:37:46 +0900

    Speed up input process (by disabling continuous option)

--- server/public/js/sound-recog.js
+++ server/public/js/sound-recog.js
@@ -1,27 +1,33 @@
 function HiSoundRecog() {
+  var self = this;
   this.recognition = new webkitSpeechRecognition();
   this.recognition.lang = 'ja-JP';
-  this.recognition.continuous = true;
-  this.recognition.interimResults = true;
   this.recognition_now = false;
 
+  this.recognition.onstart = function() {
+    console.log("[Speech Recognition] START");
+  };
+
   this.recognition.onresult = function (e) {
-    var finalText = '';
-    var interimText = '';
-    for (var i = 0; i < e.results.length; i++) {
-      if (e.results[i].isFinal) {
-        finalText += e.results[i][0].transcript;
-        hiSocket.emit("message", e.results[i][0].transcript);
-      } else {
-        interimText += e.results[i][0].transcript;
-      }
+    console.log("[Speech Recognition] RESULT");
+    console.log(e);
+    var result = e.results[e.results.length - 1];
+    if (result.isFinal) {
+      var inputMsg = result[0].transcript.trim();
+      console.log('[emit message] ' + inputMsg);
+      hiSocket.emit("message", inputMsg);
     }
     // ここのところでWebSocketにばすばす送る
-    document.getElementById('result').innerHTML = finalText;
-    document.getElementById('interim').innerHTML = interimText;
+    document.getElementById('result').innerHTML = result[0].transcript.trim();
+  };
+
+  this.recognition.onend = function() {
+    console.log("[Speech Recognition] END");
+    self.recognition.start();
   };
 
   this.recognition.onerror = function(e) {
+    console.log("[Speech Recognition] ERROR");
     console.log(e);
   };
 };
