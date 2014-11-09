4fef385caf0d44cd78f28c0c7cdfda56ff281a70
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-11-09 11:11:42 +0900

    Implement transport protocol with Kinect

--- server/kinect_connection.js
+++ server/kinect_connection.js
@@ -1,12 +1,15 @@
 var dgram = require('dgram');
 var getPnValue = require('./pn_value')
 
-var KINECT_PORT = 9999;
-var KINECT_HOST = 'localhost';
-var TYPE_ID = {
-  'like': '1',
-  'message': '2',
-}
+var KINECT_PORT = 55555;
+var KINECT_HOST = '255.255.255.255';
+// var KINECT_HOST = '172.17.0.68';
+
+// likeOn(0 or 1), hi(0 or 1), hsv(string), message
+var I_LIKE = 0;
+var I_HI = 1;
+var I_HSV = 2;
+var I_MESSAGE = 3;
 
 module.exports = new kinectConnectionConstructor();
 
@@ -14,25 +17,40 @@ module.exports = new kinectConnectionConstructor();
 function kinectConnectionConstructor() {
   this.socket = dgram.createSocket('udp4'),
 
-  this.sendText = function(text) {
-    console.log("[send via UDP] " + text);
+  this.params = [0, 0, "", ""];
 
-    var buf = new Buffer(text);
+  this.clearParams = function() {
+    this.params = [0, 0, this.params[I_HSV], ""];
+  };
+
+  this.sendParams = function() {
+    var paramString = this.params.join(',');
+    console.log("[send via UDP] " + paramString);
+
+    var buf = new Buffer(paramString);
     this.socket.send(buf, 0, buf.length, KINECT_PORT, KINECT_HOST, function(err, bytes) {
       // TODO: check status
     });
+
+    this.clearParams();
   }
 
   this.sendLike = function() {
-    var contents = [TYPE_ID.like, 'Like'];
-    this.sendText(contents.join(','));
+    this.params[I_LIKE] = 1;
+    this.sendParams();
   },
 
   this.sendMessage = function(msg) {
     var self = this;
+    this.params[I_MESSAGE] = msg;
     getPnValue(msg, function(pnValue) {
-      var contents = [TYPE_ID.message, pnValue, msg];
-      self.sendText(contents.join(','));
+      self.params[I_HSV] = self.pnValueToHSV(pnValue);
+      self.sendParams();
     });
-  }
+  };
+
+  this.pnValueToHSV = function(pnValue) {
+    // TODO: implement
+    return pnValue;
+  };
 }
