1755adb2af55b0f324621fafc0c78627654567c5
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-11-09 10:22:02 +0900

    Allow any characters to be contained in message parameter

--- pn_api/config/routes.rb
+++ pn_api/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :pn_jp_words
   namespace :api do
     namespace :v1 do
-      get '/pn_value/:message' => 'messages#pn_value'
+      get '/pn_value/:message' => 'messages#pn_value', constraints: { message: /.+/ }
     end
   end
 end
